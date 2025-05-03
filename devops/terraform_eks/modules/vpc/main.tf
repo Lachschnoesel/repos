b9f65dea80eff8f307938effd3af3d2a578958f6
Daniel Huebner <daniel_huebner@ymail.com>
2025-05-03 17:02:50 +0200

    created a  yaml for the service account for the k8s cluster and k8s yaml files to lun ch

--- devops/terraform_eks/modules/vpc/main.tf
+++ devops/terraform_eks/modules/vpc/main.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "main" {
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name                                        = "${var.cluster_name}-vpc"
@@ -7,10 +9,10 @@ resource "aws_vpc" "main" {
   }
 }
 resource "aws_subnet" "subnet_private" {
-  count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
-  availability_zone = var.availability_zone[count.index]
+  count      = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  #availability_zone = var.availability_zone[count.index]
 
   tags = {
     Name                                        = "${var.cluster_name}-private-${count.index + 1}"
